63f93a5aafa036cbb9aba00203675ae15bf50680
Gabriel Ramón <103237220+Gabriel-Ramon@users.noreply.github.com>
2022-07-25 15:07:01 -0500

    Add files via upload

---
+++ build/precache-manifest.0db6e2dbfbd5f59ed1eb896adee26605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "792ddec610235fd568a5",
+    "url": "/static/css/main.1cb69898.chunk.css"
+  },
+  {
+    "revision": "792ddec610235fd568a5",
+    "url": "/static/js/main.792ddec6.chunk.js"
+  },
+  {
+    "revision": "9d6d0ad7f97bf131636f",
+    "url": "/static/js/1.9d6d0ad7.chunk.js"
+  },
+  {
+    "revision": "49a10337ac9e134177b7",
+    "url": "/static/js/2.49a10337.chunk.js"
+  },
+  {
+    "revision": "5ebfd4f2ddfb36e0fc17",
+    "url": "/static/js/runtime~main.5ebfd4f2.js"
+  },
+  {
+    "revision": "b3e05de61940c3029f47f8400ab4cc9c",
+    "url": "/index.html"
+  }
+];
